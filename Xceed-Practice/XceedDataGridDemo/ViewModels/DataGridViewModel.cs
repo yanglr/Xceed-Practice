6656d324e5ca8d49ae4d0bb86a652a521cf89088
Enjoy233 <legege007@126.com>
2019-03-16 00:14:26 +0800

    Init the dev environment.

---
+++ Xceed-Practice/XceedDataGridDemo/ViewModels/DataGridViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using System.ComponentModel;
+
+namespace XceedDataGridDemo.ViewModels
+{
+    public class DataGridViewModel : Screen, INotifyPropertyChanged
+    {
+        private string _firstText = "Hey";
+
+        public string FirstText
+        {
+            get { return _firstText; }
+            set { _firstText = value; }
+        }
+
+    }
+}
---
+++ Xceed-Practice/XceedDataGridDemo/XceedDeploymentLicense.cs
@@ -0,0 +1,13 @@
+
+namespace Xceed.Wpf.DataGrid.Samples
+{
+  public static class XceedDeploymentLicense
+  {
+    public static void SetLicense()
+    {
+
+      // license key.
+      Xceed.Wpf.DataGrid.Licenser.LicenseKey = "DGP67-LWKYK-BAA4Y-C2BA";
+    }
+  }
+}
